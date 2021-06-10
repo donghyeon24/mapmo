9db0a1b393bdf3ebcfb5657da364518202742f14
KIM <kdhahtks@naver.com>
2021-06-10 15:11:37 +0900

    (프론트) setting 페이지 두 개 1차 완성

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mapmo.apps.MapmoConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
--- mapmo/urls.py
+++ mapmo/urls.py
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from mapmo import views
-
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('main/', include('mapmo.urls')),
-    path('setting/', include('account.urls')),
+    path('public', views.main_public, name='main_public'),
 ]
